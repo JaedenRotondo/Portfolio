7f0dc444f02e725835696fb5f3f7c554af81d0aa
JaedenRotondo <jae.rotondo@hotmail.ca>
2022-12-27 17:07:59 -0500

    Fixed PDF render error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ReactPDF from '@react-pdf/renderer';
 
 
 function Footer(){
     return (
         <div>Footer
-    ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
         </div>
     )
 }
