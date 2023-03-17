b518f3e6726a89454529b1453e59aab602ccbe48
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2023-03-17 11:33:07 -0400

    Added contact information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Contact(){
     return (
-        <div></div>
+        <div className="flex-col  pt-20 pb-6">
+            <h2 className="md:text-2xl font-large">Contact Me!</h2>
+            <li><u href="mailto:jaedenrotondo@jaedenrotondo.tech">Send Email</u></li>
+            <li><u href="https://www.linkedin.com/in/jaeden-rotondo/">View LinkedIn Profile</u></li>
+        </div>
     )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 
 function Footer(){
     return (
-        <div></div>
+        <div className="flex-col  pt-20 pb-6">
+        </div>
     )
 }
 
