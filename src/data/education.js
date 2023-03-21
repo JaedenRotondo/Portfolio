b6aeb09cba9ac32708b9d1ef583d7565d6cb8ceb
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2023-03-21 10:13:11 -0400

    added education data

---
+++ src/data/education.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Proficient in Object Oriented Programming in Java through COMP 248 and COMP 249',
+    },
+    {
+        title: 'Experience in Web Development and Programming Paradigms through SOEN 287 and COMP 348',
+    },
+    {
+        title: 'Mastered Data Structures and Algorithms through COMP 352',
+    },
+    {
+        title: 'Experience in AGILE methodologies and software development lifecycle through COMP 354',
+    },
+]
