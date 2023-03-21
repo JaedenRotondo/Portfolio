95486bb4891b093379968f57fa276d48ad5b43c9
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2023-03-21 08:44:23 -0400

    Created Timeline

---
+++ src/data/timeline.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Undergraduate COOP Student',
+        duration: '3 years',
+        details: 'I am in my third year of Computer Science at Concordia University, through my education along with my minor in Philosophy I have learned both hard skills in programming and theoretical computer science as well as soft skills in writing and project management methodologies.',
+    },
+    {
+        year: '2022',
+        title: 'Front-End Developer for Zebra Technologies',
+        duration: '4 months',
+        details: 'Worked with a team of front-end developpers and engineers to create public component level hardware and\n' +
+            'software solution documentation for the Matrox Imaging Library.\n' +
+            '• Learned about computer hardware and imaging processes, specifically how barcode imaging is done\n' +
+            '(scanning, error correction, implementation etc) with C and C++.\n' +
+            '• Debugged and tested C++ code snippets, Python scripts, and XML and XSL files.'
+    },
+
+]
